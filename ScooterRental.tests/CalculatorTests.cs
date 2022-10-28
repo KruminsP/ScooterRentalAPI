bfee946d0d997b8ab4ead442bba86016b5c45870
Peterson <55082100+Astonkajis@users.noreply.github.com>
2022-10-28 14:45:45 +0300

    Add Calculator and Scooter tests

--- ScooterRental.tests/CalculatorTests.cs
+++ ScooterRental.tests/CalculatorTests.cs
@@ -1,64 +1,64 @@
-//using System;
-//using FluentAssertions;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using ScooterRentalAPI;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScooterRentalAPI.Core.Models;
+using System;
 
-//namespace ScooterRental.tests
-//{
-//    [TestClass]
-//    public class CalculatorTests
-//    {
-//        private Calculators feeCalculator;
+namespace ScooterRental.tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        private Calculator _feeCalculator;
 
-//        [TestInitialize]
-//        public void Setup()
-//        {
-//            feeCalculator = new Calculators();
-//        }
+        [TestInitialize]
+        public void Setup()
+        {
+            _feeCalculator = new Calculator();
+        }
 
-//        [TestMethod]
-//        public void ScooterFeeCalculator_SameDayReturn_ReturnsIncome()
-//        {
-//            //Arrange
-//            var start = new DateTime(2000, 1, 1, 0, 0, 0);
-//            var end = new DateTime(2000, 1, 1, 1, 0, 0);
-//            var pricePerMinute = 0.2m;
+        [TestMethod]
+        public void ScooterFeeCalculator_SameDayReturn_ReturnsIncome()
+        {
+            //Arrange
+            var start = new DateTime(2000, 1, 1, 0, 0, 0);
+            var end = new DateTime(2000, 1, 1, 1, 0, 0);
+            var pricePerMinute = 0.2m;
 
-//            //Act
-//            var fee = feeCalculator.ScooterFeeCalculator(start, end, pricePerMinute);
+            //Act
+            var fee = _feeCalculator.ScooterFeeCalculator(start, end, pricePerMinute);
 
-//            //Assert
-//            fee.Should().Be(12);
-//        }
+            //Assert
+            fee.Should().Be(12);
+        }
 
-//        [TestMethod]
-//        public void ScooterFeeCalculator_DifferentDaysMonthsAndYears_ReturnsIncome()
-//        {
-//            //Arrange
-//            var start = new DateTime(1999, 12, 31, 23, 59, 0);
-//            var end = new DateTime(2000, 1, 1, 0, 1, 0);
-//            var pricePerMinute = 1;
+        [TestMethod]
+        public void ScooterFeeCalculator_DifferentDaysMonthsAndYears_ReturnsIncome()
+        {
+            //Arrange
+            var start = new DateTime(1999, 12, 31, 23, 59, 0);
+            var end = new DateTime(2000, 1, 1, 0, 1, 0);
+            var pricePerMinute = 1;
 
-//            //Act
-//            var fee = feeCalculator.ScooterFeeCalculator(start, end, pricePerMinute);
+            //Act
+            var fee = _feeCalculator.ScooterFeeCalculator(start, end, pricePerMinute);
 
-//            //Assert
-//            fee.Should().Be(2);
-//        }
+            //Assert
+            fee.Should().Be(2);
+        }
 
-//        [TestMethod]
-//        public void ScooterFeeCalculator_FeeExceeds20PerDay_ReturnsIncome()
-//        {
-//            //Arrange
-//            var start = new DateTime(1999, 12, 31, 0, 0, 0);
-//            var end = new DateTime(2000, 1, 1, 0, 1, 0);
-//            var pricePerMinute = 1;
+        [TestMethod]
+        public void ScooterFeeCalculator_FeeExceeds20PerDay_ReturnsIncome()
+        {
+            //Arrange
+            var start = new DateTime(1999, 12, 31, 0, 0, 0);
+            var end = new DateTime(2000, 1, 1, 0, 1, 0);
+            var pricePerMinute = 1;
 
-//            //Act
-//            var fee = feeCalculator.ScooterFeeCalculator(start, end, pricePerMinute);
+            //Act
+            var fee = _feeCalculator.ScooterFeeCalculator(start, end, pricePerMinute);
 
-//            //Assert
-//            fee.Should().Be(21);
-//        }
-//    }
-//}
+            //Assert
+            fee.Should().Be(21);
+        }
+    }
+}
--- ScooterRental.tests/ScooterTests.cs
+++ ScooterRental.tests/ScooterTests.cs
@@ -1,24 +1,24 @@
-//using FluentAssertions;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using ScooterRentalAPI.Core.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScooterRentalAPI.Core.Models;
 
-//namespace ScooterRental.tests
-//{
-//    [TestClass]
-//    public class ScooterTests
-//    {
-//        private Scooter _scooter;
+namespace ScooterRental.tests
+{
+    [TestClass]
+    public class ScooterTests
+    {
+        private Scooter _scooter;
 
-//        [TestMethod]
-//        public void ScooterCreation_IDAndPricePerMinuteSetCorrectly()
-//        {
-//            //Act
-//            _scooter = new Scooter("1", 0.2m);
+        [TestMethod]
+        public void ScooterCreation_IDAndPricePerMinuteSetCorrectly()
+        {
+            //Act
+            _scooter = new Scooter("1", 0.2m);
 
-//            //Assert
-//            _scooter.Id.Should().Be("1");
-//            _scooter.PricePerMinute.Should().Be(0.2m);
-//            _scooter.IsRented.Should().BeFalse();
-//        }
-//    }
-//}
+            //Assert
+            _scooter.Name.Should().Be("1");
+            _scooter.PricePerMinute.Should().Be(0.2m);
+            _scooter.IsRented.Should().BeFalse();
+        }
+    }
+}
